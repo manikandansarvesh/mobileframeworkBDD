bac08d2ba659137b9beae75defd2cf1f649a268e
manikandan <mani.kubaran@gmail.com>
2022-01-15 01:36:32 +0530

    first commit

---
+++ src/test/java/com/qa/reportgenerator/HTMLReporter.java
@@ -0,0 +1,52 @@
+package com.qa.reportgenerator;
+
+
+import io.cucumber.java.Scenario;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.Reportable;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HTMLReporter {
+
+    // get generated cucumber json reports names
+    public static List<String> getfilenames(){
+        File folder = new File("target/cucumber-reports");
+        File[] listOfFiles = folder.listFiles();
+        List<String> names = new ArrayList<>();
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                if(listOfFiles[i].getName().contains(".json"))
+                    names.add(listOfFiles[i].getName());
+            }
+        }
+        return names;
+    }
+
+
+
+    // To generate HTML reports from the json files
+    public static void main (String[]args){
+        File reportOutputDirectory = new File("output");
+        List<String> jsonfiles = new ArrayList<>();
+        List<String> names = getfilenames();
+        for (String string : names) {
+            jsonfiles.add("target/cucumber-reports/" + string);
+        }
+        String projectName = "cucumberProject";
+        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+        ReportBuilder reportBuilder = new ReportBuilder(jsonfiles, configuration);
+        Reportable result = reportBuilder.generateReports();
+
+    }
+
+
+
+
+
+}
+
---
+++ src/test/java/com/qa/runners/MyRunnerTest.java
@@ -0,0 +1,59 @@
+package com.qa.runners;
+
+import com.qa.reportgenerator.HTMLReporter;
+import com.qa.utils.DriverManager;
+import com.qa.utils.GlobalParams;
+import com.qa.utils.ServerManager;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.apache.logging.log4j.ThreadContext;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber-reports/cucumber.json"}
+        , features = {"src/test/resources"}
+        , glue = {"com.qa.stepdef"}
+        , snippets = CAMELCASE
+        , dryRun = false
+        , monochrome = true
+        , tags = "@validateProduct"
+)
+
+public class MyRunnerTest {
+
+    @BeforeClass
+    public static void initialize() throws Exception {
+        GlobalParams params = new GlobalParams();
+        params.initializeGlobalParams();
+
+        ThreadContext.put("ROUTINGKEY", params.getPlatformName() + "_"
+                + params.getDeviceName());
+
+        new ServerManager().startServer();
+        new DriverManager().initializeDriver();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        HTMLReporter.main(new String[]{});
+    }
+
+
+    public static void quit() {
+        DriverManager driverManager = new DriverManager();
+        if (driverManager.getDriver() != null) {
+            driverManager.getDriver().quit();
+            driverManager.setDriver(null);
+        }
+        ServerManager serverManager = new ServerManager();
+        if (serverManager.getServer() != null) {
+            serverManager.getServer().stop();
+        }
+    }
+
+
+}
